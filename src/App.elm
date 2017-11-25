8e2867324db59f40ab79bc7fdfa4a9235bbac80a
Girish S <girish.sonawane@gmail.com>
2017-11-25 15:28:45 +0530

    white pebbles are moved

--- src/App.elm
+++ src/App.elm
@@ -6,29 +6,79 @@ import Matrix.Extra
 import Maybe exposing (withDefault)
 import View exposing (view)
 import Model exposing (..)
+import Array
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        OnCellClick x y cell ->
+        OnCellClick x y currentCell ->
             let
                 newBoard =
-                    if isCurrentPlayersCell model cell then
+                    if isCurrentPlayersCell model currentCell then
                         model.board
-                            |> Matrix.map (\c -> { c | state = Normal })
-                            |> Matrix.set y x ({ cell | state = Selected })
+                            |> Matrix.map (\cell -> { cell | state = Normal })
+                            |> Matrix.set y x ({ currentCell | state = Selected })
                             |> Matrix.indexedMap
-                                (\j i c ->
-                                    if isNeighbour i j x y && c.pebble == Nothing then
-                                        { c | state = ValidMove }
+                                (\j i cell ->
+                                    if isNeighbour i j x y && cell.pebble == Nothing then
+                                        { cell | state = ValidMove }
                                     else
-                                        c
+                                        cell
                                 )
+                    else if
+                        isAnyPebbleSelected model
+                            && currentCell.pebble
+                            == Nothing
+                            && currentCell.state
+                            == ValidMove
+                    then
+                        let
+                            _ =
+                                Debug.log "ix array: " (toString (Matrix.toIndexedArray model.board))
+
+                            ( ( i, j ), selectedCell ) =
+                                getSelectedPebbleXY model.board
+                        in
+                            movePebble x y i j selectedCell currentCell model
                     else
                         model.board
             in
                 ( { model | board = newBoard }, Cmd.none )
+
+
+getSelectedPebbleXY board =
+    let
+        selectedCells =
+            Matrix.toIndexedArray board
+                |> Array.filter
+                    (\( ( y, x ), cell ) ->
+                        cell.state == Selected
+                    )
+
+        _ =
+            Debug.log "selectedCell" selectedCells
+    in
+        case Array.get 0 selectedCells of
+            Just ( ( y, x ), cell ) ->
+                ( ( y, x ), cell )
+
+            Nothing ->
+                ( ( -1, -1 ), emptyCell )
+
+
+movePebble to_i to_j from_x from_y from_cell to_cell model =
+    model.board
+        |> Matrix.set to_j to_i ({ to_cell | pebble = from_cell.pebble })
+        |> Matrix.set from_x from_y ({ from_cell | pebble = Nothing })
+        |> Matrix.map (\cell -> { cell | state = Normal })
+
+
+isAnyPebbleSelected model =
+    model.board
+        |> Matrix.filter (\c -> c.state == Selected)
+        |> Array.isEmpty
+        |> not
 
 
 isNeighbour i j x y =
